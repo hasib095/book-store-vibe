56b7035099cfa743dc31989ac6b0a204cf9568a7
Jahid Hasib <cse1095.hasib@gmail.com>
2026-09-24 02:05:11 +0600

    banner section added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
+
+//https://designobserver.com/wp-content/uploads/2024/06/40406-sa-hero.png
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'designobserver.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
